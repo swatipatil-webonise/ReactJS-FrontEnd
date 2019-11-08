2ec3eb8b10649ea421f9c79898106e24e3fe6841
swati <swati.patil@weboniselab.com>
2019-11-08 20:49:20 +0530

    add renderTodoList method in ListTodo to render todolist

--- TodoWithReact/README.md
+++ TodoWithReact/README.md
@@ -23,18 +23,13 @@ Applications needed :
     3) Backend which provides api's for all todo app on localhost:8080.
 
 Softwares used : 
-    a) Visual studio code.
-    b) npm (6.11.3)
-    c) node (V12.10.0)
+    a) npm (6.11.3)
+    b) node (V12.10.0)
 
 To run this project : 
-1) Extract the zip in visual studio's workspace.
-2) Open the project in visual studio code.
-3) Open terminal.
-4) Npm install . 
-5) Npm start .
-6) Open [http://localhost:3000](http://localhost:3000) to view it in the browser .
-7) Right click To inspect code , See console to see errors if any.
+1) Check out to the repositary and execute following commands : 
+ npm install  
+ npm start 
 
 Learn More : 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
--- TodoWithReact/src/ListTodo.js
+++ TodoWithReact/src/ListTodo.js
@@ -1,32 +1,31 @@
 import React from 'react';
 
-export const ListTodo = ({
-  todos,
-  onEdit,
-  onDelete,
-}) => {
-  return (
-    <div>
+export class ListTodo extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  renderTodoList() {
+    return this.props.todos.map(todo =>
+      <tr key={todo.id}>
+        <td>{todo.id}</td>
+        <td>{todo.desc}</td>
+        <td><button onClick={this.props.onEdit.bind(null, todo.id)}>Edit</button></td>
+        <td><button onClick={this.props.onDelete.bind(null, todo.id)}>Delete</button></td>
+      </tr>
+    )
+  }
+
+  render() {
+    return (
       <table border="1">
         <thead>
-          <tr>
-            <th>Id</th>
-            <th>Task to perform</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
+          <tr><th>Id</th><th>Task to perform</th><th>Edit</th><th>Delete</th></tr>
         </thead>
         <tbody>
-          {todos && todos.map(todo =>
-            <tr key={todo.id}>
-              <td>{todo.id}</td>
-              <td>{todo.desc}</td>
-              <td><button onClick={onEdit.bind(null, todo.id)}>Edit</button></td>
-              <td><button onClick={onDelete.bind(null, todo.id)}>Delete</button></td>
-            </tr>
-          )}
+          {this.renderTodoList()}
         </tbody>
       </table>
-    </div>
-  );
-};
+    );
+  }
+}
